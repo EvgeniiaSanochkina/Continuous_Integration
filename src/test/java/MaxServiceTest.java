552a1f9288f3a14699dbe8af3f2c1b77bb2cbbe8
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2023-11-20 12:32:32 +0700

    mavenFixed

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MaxServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+        MaxService service = new MaxService();
+        int a = 5;
+        int b = 6;
+        int expected = b;
+        int actual = service.max(a, b);
+        assertEquals(expected, actual);
+    }
 }
